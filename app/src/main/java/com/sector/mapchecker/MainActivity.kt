d41fecf5d443cd8484dd35952d867b52e9b5f5cc
sector <75115941+onesector@users.noreply.github.com>
2023-04-13 21:40:53 +0300

    Initial commit

---
+++ app/src/main/java/com/sector/mapchecker/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.sector.mapchecker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sector.mapchecker.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
